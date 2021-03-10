8393189419a510e2fe7a92ced20ad84beae167e0
DiamondBlood <almaz777aminov@gmail.com>
2021-03-10 15:13:38 +0300

    Вид и контроллер для корзины

---
+++ GGS/GGS.Domain/Cart.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+
+namespace GGS.Domain
+{
+    public class Cart
+    {
+        private List<CartLine> lineCollection = new List<CartLine>();
+
+
+        public void AddItem(Game game, int quantity)
+        {
+            CartLine line = lineCollection
+                .Where(g => g.Game.ID == game.ID)
+                .FirstOrDefault();
+
+            if (line == null)
+            {
+                lineCollection.Add(new CartLine
+                {
+                    Game = game,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public void RemoveLine(Game game)
+        {
+            lineCollection.RemoveAll(l => l.Game.ID == game.ID);
+        }
+
+        public decimal ComputeTotalValue()
+        {
+            return lineCollection.Sum(e => e.Game.PriceRub * e.Quantity);
+
+        }
+        public void Clear()
+        {
+            lineCollection.Clear();
+        }
+
+        public IEnumerable<CartLine> Lines
+        {
+            get { return lineCollection; }
+        }
+    }
+
+    public class CartLine
+    {
+        public Game Game { get; set; }
+        public int Quantity { get; set; }
+    }
+
+
+}
---
+++ GGS/GGS/Controllers/CartController.cs
@@ -0,0 +1,76 @@
+﻿using GGS.Data;
+using GGS.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using GGS.Web.Models;
+
+namespace GGS.Web.Controllers
+{
+
+    public class CartController : Controller
+    {
+        private ISession _session;
+        private readonly ILogger<CartController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public CartController(ILogger<CartController> logger, ApplicationDbContext context, IHttpContextAccessor httpContextAccessor)
+        {
+            _logger = logger;
+            _context = context;
+            this._session = httpContextAccessor.HttpContext.Session;
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
+
+        public RedirectToRouteResult AddToCart(int gameId, string returnUrl)
+        {
+            Game game = _context.Games
+                .FirstOrDefault(g => g.ID == gameId);
+
+            if (game != null)
+            {
+                GetCart().AddItem(game, 1);
+            }
+            return RedirectToRoute("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int gameId, string returnUrl)
+        {
+            Game game = _context.Games
+                .FirstOrDefault(g => g.ID == gameId);
+
+            if (game != null)
+            {
+                GetCart().RemoveLine(game);
+            }
+            return RedirectToRoute("Index", new { returnUrl });
+        }
+
+        public Cart GetCart()
+        {
+            Cart cart;
+            cart = new Cart();
+            /*
+            _session.Set("Cart",cart);
+            if (cart == null)
+            {
+                cart = new Cart();
+                
+                
+            }*/
+            return cart;
+        }
+    }
+}
